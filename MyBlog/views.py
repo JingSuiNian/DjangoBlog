3aada77648e9d0543c61d57d9d36be9578148684
JingsuiNian <403610970@qq.com>
2016-06-03 17:39:01 +0800

    aa

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+#from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
