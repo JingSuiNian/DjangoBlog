b982944adec27918cd10162fd5ece2f79a12468e
JingsuiNian <403610970@qq.com>
2016-06-07 15:11:19 +0800

    a

--- BlogNian/wsgi.py
+++ BlogNian/wsgi.py
@@ -12,6 +12,9 @@ import sys
 
 sys.path.append("/usr/local/lib/python2.7/site-packages/Django-1.7.11-py2.7.egg/")
 sys.path.append("/usr/local/lib/python2.7/site-packages/MySQLdb/")
+sys.path.append("/usr/local/lib/python2.7/site-packages/pymysql/")
+sys.path.append("/usr/local/lib/python2.7/site-packages/PyMySQL-0.7.4.dist-info/")
+sys.path.append("/usr/local/lib/python2.7/site-packages/MySQL_python-1.2.5.dist-info/")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BlogNian.settings")
 
