ae5dd77981e5e8213ab129744c6f5aad7e993517
JingsuiNian <403610970@qq.com>
2016-06-07 11:53:17 +0800

    a

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def hello(request):
     context = {}
-    context['hello'] = 'Hello World!'
+    context['hello'] = 'Hello World'
     return render(request, 'hello.html', context)
