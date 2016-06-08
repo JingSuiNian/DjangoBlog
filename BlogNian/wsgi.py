85c6bbfe722b6ea974b433d4f2fb807261d3c572
JingsuiNian <403610970@qq.com>
2016-06-09 01:24:34 +0800

    aaa

--- BlogNian/wsgi.py
+++ BlogNian/wsgi.py
@@ -7,15 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
 import sys
 
-sys.path.append("/usr/local/lib/python2.7/site-packages/Django-1.7.11-py2.7.egg/")
 sys.path.append("/usr/local/lib/python2.7/site-packages/pymysql/")
+sys.path.append("/usr/local/lib/python2.7/site-packages/Django-1.7.11-py2.7.egg/")
+
+import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BlogNian.settings")
 
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 from django.core.wsgi import get_wsgi_application
