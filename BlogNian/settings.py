d77be64fbc98afaacf465fb719d2482709fd2621
JingsuiNian <403610970@qq.com>
2016-06-09 11:58:42 +0800

    aaa

--- BlogNian/settings.py
+++ BlogNian/settings.py
@@ -66,10 +66,13 @@ DATABASES = {
     }
 }
 
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + "/templates", ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
